34999bd52e57c81b89ab5145b494a112296e8967
Angelo Corrao <precipitevoliss1@gmail.com>
2022-11-28 15:10:29 +0100

    Adjustment of the ball's bouncingness

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Player1 : Paddle {
 
-	private Vector3 direction;
-
 	// Start is called before the first frame update
 	void Start() {
 
@@ -13,12 +11,23 @@ public class Player1 : Paddle {
 
 	// Update is called once per frame
 	void Update() {
-		direction = new Vector3(0, Input.GetAxisRaw("Vertical"), 0);
+		if(Input.GetKey(KeyCode.W)) {
+			if (transform.position.y < 4.2)
+				transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.S)) {
+			if (transform.position.y > -4.2)
+				transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+		/*direction = new Vector3(0, Input.GetAxisRaw("Vertical"), 0);
+		if(transform.position.y > -5 && transform.position.y < 5)
+			transform.position += direction * speed * Time.deltaTime;*/
 	}
 
 	private void FixedUpdate() {
-		//transform.position += direction * speed * Time.fixedDeltaTime;
-		rb.velocity = direction * speed;
+		//rb.MovePosition(direction);
+		//rb.velocity = direction * speed;
 		/*if (direction.sqrMagnitude != 0)
 			rb.AddForce(direction * speed);*/
 	}
--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player2 : Paddle {
-	private Vector3 direction;
 
 	// Start is called before the first frame update
 	void Start() {
@@ -12,13 +11,14 @@ public class Player2 : Paddle {
 
 	// Update is called once per frame
 	void Update() {
-		direction = new Vector3(0, Input.GetAxisRaw("Vertical2"), 0);
-	}
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			if (transform.position.y < 4.2)
+				transform.position += Vector3.up * speed * Time.deltaTime;
+		}
 
-	private void FixedUpdate() {
-		transform.position += direction * speed * Time.deltaTime;
-		//rb.velocity = direction * speed;
-		/*if (direction.sqrMagnitude != 0)
-			rb.AddForce(direction * speed);*/
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			if (transform.position.y > -4.2)
+				transform.position += Vector3.down * speed * Time.deltaTime;
+		}
 	}
 }
